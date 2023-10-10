3fbc25ebdd971a0d5c00c391b46b6d46f33c8e2a
Shakkya Ranasinghe <shakkyaenvy@gmail.com>
2023-10-10 18:52:53 -0500

    hpcc compatible codes and logs

--- Code/multiD.R
+++ Code/multiD.R
@@ -3,17 +3,25 @@
 
 rm(list = ls())
 # require(tidyverse)
-library(here)
-
-i_am("Code/multiD.Rmd")
-source(here("Code", "utils.R"))
-config <- config::get()
 
 args <- commandArgs(TRUE)
-parameters <- as.numeric(args)
+parameters <- as.numeric(args[1:4])
 n = parameters[1]
 sd = parameters[2]
 seed = parameters[3]
+jobid = parameters[4]
+jobname = args[5]
+
+here::i_am("Code/multiD.R")
+library(here)
+library(logger, quietly = TRUE)
+
+source(here("Code", "utils.R"))
+config <- config::get()
+
+date_time = format(Sys.time(), "%H_%M_%b_%d_%Y")
+file_name = f("{jobname}_{jobid}_{date_time}")
+log_appender(appender_file(here(config$path_to_logs, f("{file_name}.log"))))
 
 evalHere <- function(x) {  # since we're going to be using this heavily. A wrapper
   if(!is.character(x)) stop("x should be a string")
@@ -22,11 +30,14 @@ evalHere <- function(x) {  # since we're going to be using this heavily. A wrapp
 
 
 ## ----model--------------------------------------
+log_info("-----------------------------------------------------")
+log_info(f("Process Starting: multiDimensional Kernel Regression Metrics Evaluation"))
+log_info(f("Parameters Received: {parameters}") %>% skip_formatter())
+
 set.seed(seed)
 
 n = n
 functionName <- "DGP1"
-print(formals(functionName))
 
 modelVals = modelSp(functionName, n = n, sd = sd) # from utils # x returning as matrix, fx as list
 xargs = modelVals$xargs
@@ -35,7 +46,7 @@ fx = modelVals$fx
 
 xDim = min(dim(x))
 for (i in 1:xDim) { # assign each row to x1, x2 ...>
-  assign(f("x{i}"), x[i, ] |> unlist())
+  assign(f("x{i}"), x[i, ] %>% unlist())
 }
 
 
@@ -53,13 +64,10 @@ Rkernel = c()
 
 for (i in 1:xDim) { # R = list(R1, R2)
   xi = evalHere(f("x{i}"))
-  print(f("Calculating outer Rkernerl for <x{i}, x{i}>"))
+  log_info(f("Calculating outer Rkernerl for <x{i}, x{i}>"))
   assign(f("Rkernel{i}"), outer(xi, xi, bernoulliKernel))
-  Rkernel[f("Rkernel{i}")] = evalHere(f("Rkernel{i}")) |> list()
+  Rkernel[f("Rkernel{i}")] = evalHere(f("Rkernel{i}")) %>% list()
 }
-
-print(names(Rkernel))
-
 
 ## ----kernel and R-------------------------------
 mprod <- function(kernel, xdim, name, I) {
@@ -74,25 +82,24 @@ R = mprod(kernel = Rkernel, xdim = xDim, name = "Rkernel", I = I)
 
 
 ## ----fHat---------------------------------------
-coef <- (R + n*lambda*I) |> GInv()
+coef <- (R + n*lambda*I) %>% GInv()
 coef <- coef %*% matrix(fx)
 
 phi <- R
 
-fHat <- {t(phi) %*% coef } |> c()
+fHat <- {t(phi) %*% coef } %>% c()
 
 
 ## ----metrics------------------------------------
 fHat_rmse = Metrics::rmse(fHat, fx)
-print(f("Calculated RMSE: {round(fHat_rmse, 2)}"))
+log_info(f("Calculated RMSE: {round(fHat_rmse, 2)}"))
 
 # write the metrics to a file
 valsList = list(n = n, sd = sd, seed = seed, rmse = fHat_rmse)
-file_name = format(Sys.time(), "%b_%d_%Y") |> as.character()
-readr::write_csv(as.data.frame(valsList), file = here("Data", "multiDimension_{file_name}.csv"), append = TRUE, col_names = TRUE)
+readr::write_csv(as.data.frame(valsList), file = here("Data", f("{file_name}.csv")), append = TRUE, col_names = FALSE)
 
-print(f("File saved: {config$path_to_data}{.Platform$file.sep}multiDimension_{file_name}.csv"))
-
+log_info(f("File saved: {config$path_to_data}{.Platform$file.sep}{file_name}.csv"))
+log_info(f("File has colnames: {names(valsList)}") %>% logger::skip_formatter())
 
 ## ----visualizex1--------------------------------
 # require(dplyr)
@@ -122,5 +129,5 @@ print(f("File saved: {config$path_to_data}{.Platform$file.sep}multiDimension_{fi
 # 
 
 ## -----------------------------------------------
-print("done with this")
+log_info("Code Finalized")
 
