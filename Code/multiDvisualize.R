35a8ea69ab2ab4bdcfdce3386ca1736ebdbd9f69
Shakkya Ranasinghe <shakkyaenvy@gmail.com>
2023-10-11 22:15:41 -0500

    corrected gcv rmarkdown file. And generating visualizations from data file. With SCP code

--- Code/multiDvisualize.R
+++ Code/multiDvisualize.R
@@ -6,13 +6,14 @@ library(ggplot2)
 ## Execute following in terminal while in Data folder
 ## scp sranasin@quanah.hpcc.ttu.edu:/home/sranasin/kernelRidgeRegression/Data/datamultiD.csv .
 
-file_name = "dataMultiDOct11.csv"
+file_name = "RealTest1.csv"
 df = readr::read_csv(here("Data", file_name), 
                      col_names = c("n", "sd", "seed", "rmse"))
 df %>% 
   head()
 
-df %>% 
-  select(rmse) %>% summary()
-  ggplot(aes(rmse)) + 
+df %>%
+  filter(n == 200) %>%
+  select(rmse) %>%
+  ggplot(aes(y = rmse)) +
   geom_boxplot()
