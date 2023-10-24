1da8356b1f612431703a855c414f50a8e0bef85d
Shakkya Ranasinghe <shakkyaenvy@gmail.com>
2023-10-23 22:44:52 -0500

    Running in HPCC

--- Code/multiD.R
+++ Code/multiD.R
@@ -2,15 +2,15 @@
 # knitr::opts_chunk$set(echo = TRUE)
 
 rm(list = ls())
-n=100;sd=0.01;seed=12;jobid=010;jobname="testAll";runTimeName="newLocalRun";parameters = c(1, 2)
-# args <- commandArgs(TRUE)
-# parameters <- as.numeric(args[1:4])
-# n = parameters[1]
-# sd = parameters[2]
-# seed = parameters[3]
-# jobid = parameters[4]
-# runTimeName = args[5]
-# jobname = args[6]
+# n=100;sd=0.01;seed=12;jobid=010;jobname="testAll";runTimeName="newLocalRun";parameters = c(1, 2)
+args <- commandArgs(TRUE)
+parameters <- as.numeric(args[1:4])
+n = parameters[1]
+sd = parameters[2]
+seed = parameters[3]
+jobid = parameters[4]
+runTimeName = args[5]
+jobname = args[6]
 
 here::i_am("Code/multiD.R")
 suppressPackageStartupMessages(library(here, quietly = TRUE))
@@ -37,7 +37,7 @@ log_info(f("Parameters Received: {paste(parameters, collapse = ', ')}"))
 set.seed(seed)
 
 n = n
-functionName <- "DGP1"
+functionName <- "DGP2"
 
 modelVals = modelSp(functionName, n = n) # from utils # x returning as matrix, fx as list
 xargs = modelVals$xargs
