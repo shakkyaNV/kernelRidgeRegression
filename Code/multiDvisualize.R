6d2dbf7d2f6e54f2f8009ce001eafb0606e8b0a6
Shakkya Ranasinghe <shakkyaenvy@gmail.com>
2023-11-04 17:24:12 -0500

    day to day changes. Doesn't matter

--- Code/multiDvisualize.R
+++ Code/multiDvisualize.R
@@ -38,32 +38,29 @@ df |>
 
 
 
-
-
-
 #############################
 ########### DGP2
 
-df = readr::read_csv(here("Data", "HPCC_DGP2.csv"), 
+df1 = readr::read_csv(here("Data", "HPCC_DGP2.csv"), 
                      col_names = c("id", "rmse", "V1", "V2", "V3", "V4", "V5", "V6", "n", "sd", "seed"), 
                      col_types = list("c", "d", "c","c","c","c","c","c","n","d","n"))
-df |> 
+df1 |> 
   glimpse()
 
-df |> 
+df1 |> 
   select(id, starts_with("V"), rmse) |> 
-  tidyr::unite("degree", V1:V6) -> df_c
+  tidyr::unite("degree", V1:V6) -> df1_c
 
-df_c |> 
+df1_c |> 
   group_by(id, degree) |> 
   summarise(mean = mean(rmse)) |> 
-  arrange(mean) -> df_m
+  arrange(mean) -> df1_m
 
-df_m
+df1_m
 
 
-df |> 
-  left_join(y = df_m, by = "id") |> 
+df1 |> 
+  left_join(y = df1_m, by = "id") |> 
   select(-starts_with("V")) |> 
   mutate(id = forcats::fct_reorder(id, rmse, .fun = 'mean'))  |> 
   arrange(mean) |> 
@@ -73,33 +70,37 @@ df |>
   # ylim(0, 5) + 
   theme(axis.text.x = element_text(angle = 45, vjust = 0.5, hjust=1))
 
+
+
 ########### DGP1
 
-df = readr::read_csv(here("Data", "HPCC_DGP1.csv"), 
+
+
+df2 = readr::read_csv(here("Data", "HPCC_DGP1.csv"), 
                      col_names = c("id", "rmse", "V1", "V2", "V3", "n", "sd", "seed"), 
                      col_types = list("c", "d", "c","c","c","c","c","c","n","d","n"))
-df |> 
+df2 |> 
   glimpse()
 
-df |> 
+df2 |> 
   select(id, starts_with("V"), rmse) |> 
-  tidyr::unite("degree", V1:V3) -> df_c
+  tidyr::unite("degree", V1:V3) -> df2_c
 
-df_c |> 
+df2_c |> 
   group_by(id, degree) |> 
   summarise(mean = mean(rmse)) |> 
-  arrange(mean) -> df_m
+  arrange(mean) -> df2_m
 
-df_m
+df2_m
 
 
-df |> 
-  left_join(y = df_m, by = "id") |> 
+df2 |> 
+  left_join(y = df2_m, by = "id") |>
   select(-starts_with("V")) |> 
   mutate(id = forcats::fct_reorder(id, rmse, .fun = 'mean'))  |> 
   arrange(mean) |> 
   head(318*4) |> 
   ggplot() + 
   geom_boxplot(aes(x = reorder(degree, mean), y = rmse, fill = id)) + 
-  # ylim(0, 5) + 
+  ylim(0, 5) + 
   theme(axis.text.x = element_text(angle = 45, vjust = 0.5, hjust=1))
