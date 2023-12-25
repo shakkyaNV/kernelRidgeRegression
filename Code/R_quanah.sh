686c829ac9646b0d47646edab6ce293b0617c1ad
shakkyaNV <shakkyaenvy@gmail.com>
2023-12-25 13:04:51 -0600

    run level changes

--- Code/R_quanah.sh
+++ Code/R_quanah.sh
@@ -10,7 +10,7 @@ module load intel R
 
 for n in 100 150
 do
-	for multiplier in `seq -1 0.1 1`
+	for multiplier in `seq 0.1 -0.01 -0.1`
 	do
 		Rscript $PATH_TO_KRR/Code/$filename.R $n $sd $randomseed $SLURM_JOB_ID $multiplier $runTimeName $SLURM_JOB_NAME;
 	done
