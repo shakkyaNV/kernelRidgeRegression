191e09eb172f6428bfad316bfa245b0dfa8f67f7
shakkyaNV <shakkyaenvy@gmail.com>
2024-02-13 12:54:33 -0600

    increase sd, instances

--- Code/R_quanah.sh
+++ Code/R_quanah.sh
@@ -4,13 +4,13 @@ source ~/.bashrc
 randomseed=$1
 filename=$2
 runTimeName=$3
-sd=0.01
+sd=0.5
 
 module load intel R
 
 for n in 100 150
 do
-	for multiplier in `seq 0.1 -0.01 -0.1`
+	for multiplier in `seq 0.005 -0.0005 -0.005`
 	do
 		Rscript $PATH_TO_KRR/Code/$filename.R $n $sd $randomseed $SLURM_JOB_ID $multiplier $runTimeName $SLURM_JOB_NAME;
 	done
