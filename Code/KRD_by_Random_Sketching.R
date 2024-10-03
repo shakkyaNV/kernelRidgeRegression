3c78447e5b2dde8c9b384f85c27520e36bcd9f69
Ranasinghe <Shakkya.Ranasinghe@ttu.edu>
2024-10-03 00:47:05 -0500

    Random SKetching matrix approach

---
+++ Code/KRD_by_Random_Sketching.R
@@ -0,0 +1,75 @@
+here::i_am("Code/KRD_by_Random_Sketching.R")
+require(here)
+require(CVXR)
+require(rlang); require(magrittr)
+
+# Setup
+set.seed(1)
+n = 200
+k = 100
+
+source(here("Code", "utils.R"))
+source(here("Code", "Accept_Reject.R"))
+
+
+# Create the simulation realizations for p(x) = exp(f(x))
+p_x_all = sim.fun(n)
+x = p_x_all$realization
+
+# create bold(R) = R[Xi, Xj]
+R = outer(x, x, FUN = bernoulliKernel) 
+
+# Create phi_x = R[Xi, x]
+phi = outer(runif(1000), x, FUN = bernoulliKernel)
+lambda = 0.000001
+
+# Create a random sketching MATRIX
+sketching_matrix <- matrix(rnorm(k*n), nrow=k, ncol=n)
+S <- sketching_matrix
+
+# USE 3rd equation from paper
+# So we'll solve this argmin using CVXR
+c = CVXR::Variable(k)
+objective = {-sum((R %*% t(S)) %*% c)/n} +
+  { {(phi%*%t(S)) %*% c} %>% exp() %>% mean()} + 
+  { lambda * quad_form(t(S)%*%c, R)}
+
+problem = Problem(Minimize(objective))
+result = solve(problem)
+c_hat_KRD = result$getValue(c)
+
+
+# now that we have the RKD estimator for c, We can plug into our defined function
+# as a kernel ridge density estimator
+x_new = seq(0, 1, length.out = 100)
+temp_y = outer(x_new, x, FUN = bernoulliKernel)
+fitted_values = temp_y %*% c_hat_KRD ### Dimension Error
+
+
+# Actual values from the function itself
+p_x = p(x_new)
+
+
+# Hist
+# hist(log(p_x) - mean(log(p_x)), probability = TRUE, breaks = 50,
+#      main = "Histogram of RKD Estimator: (Blue is RKD)")
+# lines(newx, exp(fitted_values - mean(fitted_values)), 
+#       type = 'l', col = "blue")
+
+
+# Plot 
+plot(x_new, fitted_values - mean(fitted_values), type = "l", col = "red")
+lines(x_new, f(x_new) - mean(f(x_new)), col = "blue")
+title("Blue is function f(x) values, Red is RKD")
+
+
+
+
+
+
+
+
+
+
+
+
